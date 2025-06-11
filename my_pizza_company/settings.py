a8af3250dc82bbb5ed5cae228facade60fe740f5
Nguyễn Viết Trung <trungpho2006@gmail.com>
2025-06-11 15:47:07 +0700

    Reup project

--- my_pizza_company/settings.py
+++ my_pizza_company/settings.py
@@ -125,3 +125,19 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django.server': {
+            'handlers': ['null'],
+            'level': 'INFO',  # hoặc 'WARNING' hoặc 'ERROR'
+            'propagate': False,
+        },
+    },
+}
